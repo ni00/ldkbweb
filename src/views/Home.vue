9a13cc2b0f4e7f56d5e25d81302c2c5a7be7353a
mail@nixiaobai.com <mail@nixiaobai.com>
2021-02-19 15:34:00 +0800

    modify livere

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -99,22 +99,23 @@ export default {
     }
   },
   beforeMount() {
-    (function(d, s) {
-      var j,
-        e = d.getElementsByTagName(s)[0];
+    // (function(d, s) {
+    //   var j,
+    //     e = d.getElementsByTagName(s)[0];
 
-      if (typeof LivereTower === "function") {
-        return;
-      }
+    //   if (typeof LivereTower === "function") {
+    //     return;
+    //   }
 
-      j = d.createElement(s);
-      j.src = "https://cdn-city.livere.com/js/embed.dist.js";
-      j.async = true;
+    //   j = d.createElement(s);
+    //   j.src = "https://cdn-city.livere.com/js/embed.dist.js";
+    //   j.async = true;
 
-      e.parentNode.insertBefore(j, e);
-    })(document, "script");
+    //   e.parentNode.insertBefore(j, e);
+    // })(document, "script");
 
     loadJS("//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js");
+    loadJS("https://cdn-city.livere.com/js/embed.dist.js")
   },
 
   computed: {
